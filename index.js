7fe1ea17a3200b83d7c88fb25c75e12adf30c94a
tgifford-usc <140787784+tgifford-usc@users.noreply.github.com>
2023-09-06 10:21:18 +1000

    Reverted back to mDNS name

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const sendButton = document.getElementById("sendButton");
 const msgTextBox = document.getElementById("msgTextBox");
 const dataResultArea = document.getElementById("mainResults");
 
-// const endpoint = "https://esp8266.local";
+const endpoint = "https://esp8266.local";
 // const endpoint = "https://192.168.231.211"
-const endpoint = "https://192.168.20.12";
+// const endpoint = "https://192.168.20.12";
 
 // Make a GET api request
 async function sendMessage(msg) {
