9da7d665a367d8b943f03600d990f2fffe1256a8
tgifford-usc <140787784+tgifford-usc@users.noreply.github.com>
2023-09-25 16:03:27 +1000

    Added polling
    Polls for data on the webserver by visiting the /poll endpoint

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 // make variables for the html elements
 const sendButton = document.getElementById("sendButton");
+const pollButton = document.getElementById("pollButton");
 const msgTextBox = document.getElementById("msgTextBox");
 const dataResultArea = document.getElementById("mainResults");
 
 const endpoint = "https://esp8266.local";
 // const endpoint = "https://192.168.231.211"
 // const endpoint = "https://192.168.20.12";
+
+// Polling for data on the microcontroller
+let polling = false;
+let pollInterval = 5000;
 
 // Make a GET api request
 async function sendMessage(msg) {
@@ -15,6 +20,12 @@ async function sendMessage(msg) {
     // Fetch a response from the REST API
     const response = await fetch(`${endpoint}?msg=${msg}`);
     
+    // bail out if the response status was anything other than success
+    if (response.status != 200) { 
+        console.log(`${endpoint}/poll returned status ${response.status}`);
+        return;
+    } 
+
     // extract the JSON payload from the response
     const result = await response.json();
     
@@ -31,6 +42,43 @@ async function sendMessage(msg) {
 }
 
 
+// See if the microcontroller has some data it wants to send
+async function queryForData() {
+    // Fetch a response from the REST API
+    const response = await fetch(`${endpoint}/poll`);
+    
+    // bail out if the response status was anything other than success
+    if (response.status != 200) { 
+        console.log(`${endpoint}/poll returned status ${response.status}`);
+        return;
+    } 
+
+    // extract the JSON payload from the response
+    const result = await response.json();
+    
+    // Sometimes the payload is actually a string instead of an object. If so convert
+    // it to an object
+    let objResult = (typeof result == "object") ? result  : JSON.parse(result);
+    
+    // and either way, convert the object to a string to print out. 
+    let strResult = JSON.stringify(objResult, undefined, 2);
+    dataResultArea.innerHTML = `<pre><code>${strResult}</code></pre>`;
+    
+    // return the JSON object
+    return objResult;
+}
+
+// Poll regularly for data
+async function pollForData() {
+    let res = queryForData();
+    if (polling) {
+        setTimeout(pollForData, pollInterval);
+    }
+    return res;
+}
+
+
+
 // Make the GET request when the fetch button is clicked
 sendButton.addEventListener('click', async (event) => {
     // fetch whatever URL has been typed into textbox
@@ -45,4 +93,19 @@ msgTextBox.addEventListener('change', async (event) => {
     let msg = msgTextBox.value;
     let result = await sendMessage(msg);
     // Do something else with 'result' if you want
+});
+
+
+// toggle polling when the POLL button is pressed
+pollButton.addEventListener('click', (event) => {
+    if (polling) {
+        polling = false;
+        pollButton.classList.remove('toggled-on');
+        pollButton.classList.add('toggled-off');
+    } else {
+        polling = true;
+        pollForData();
+        pollButton.classList.remove('toggled-off');
+        pollButton.classList.add('toggled-on');
+    }
 });
